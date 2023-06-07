f85546dc730eacea982466702207886b922a3d23
Abhishek Sharma <abhishek79009@gmail.com>
2023-06-07 11:36:19 +0530

    v1.0 try2

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ dotenv.config('./.env')
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const cloudinary = require('cloudinary').v2;
 
-// app.use(morgan('common'))
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
 
 let origin = 'http://localhost:3000';
 
-if (process.env.NODE_ENV=== "production") {
+if (process.env.NODE_ENV?.trim() === "production") {
   origin = process.env.CLIENT_URL
 }
 app.use(cors({
   credentials: true,
   origin
 }))
+
+
 
 
 
