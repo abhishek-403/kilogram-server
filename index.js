0c1fcb3cef298c5cbfe0ac47285d1d956e010d42
Abhishek Sharma <abhishek79009@gmail.com>
2023-06-07 11:22:02 +0530

    v1.0 deploy1

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ const cloudinary = require('cloudinary').v2;
 // app.use(morgan('common'))
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
+
+let origin = 'http://localhost:3000';
+
+if (process.env.NODE_ENV=== "production") {
+  origin = process.env.CLIENT_URL
+}
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin
 }))
 
 
