074728cbc5393663a48320ce70870eec9ec2ef86
Abhishek Sharma <abhishek79009@gmail.com>
2024-07-19 13:40:53 +0530

    healthy server endpoint added

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
-const dotenv = require('dotenv')
-dotenv.config('./.env')
+const dotenv = require("dotenv");
+dotenv.config("./.env");
 
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const cookieParser = require('cookie-parser')
-const cloudinary = require('cloudinary').v2;
+const cookieParser = require("cookie-parser");
+const cloudinary = require("cloudinary").v2;
 
-app.use(express.json({ limit: '10mb' }))
-app.use(cookieParser())
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
 
-let origin = 'http://localhost:3000';
+let origin = "http://localhost:3000";
 
 if (process.env.NODE_ENV?.trim() === "production") {
-  origin = process.env.CLIENT_URL
+  origin = process.env.CLIENT_URL;
 }
-app.use(cors({
-  credentials: true,
-  origin
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin,
+  })
+);
 
-
-
-
-
-// Configuration 
+// Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-
-
-const mongoDb = require('./dbConnect')
+const mongoDb = require("./dbConnect");
 mongoDb();
-
 
 const port = process.env.PORT || 4001;
 
-const mainRouter = require('./routers')
-
-app.use('/api', mainRouter)
+const mainRouter = require("./routers");
+app.get("/", (req, res) => {
+  return res.send("healthy server");
+});
+app.use("/api", mainRouter);
 
 app.listen(port, () => {
   console.log("Listening at", port);
-})
+});
